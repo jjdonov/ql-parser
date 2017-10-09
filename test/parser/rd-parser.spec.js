b8f430a21d414dbedcbc7011755633e254fb97f0
John Donovan <john.donovan@gtnexus.com>
2017-10-09 15:47:03 -0700

    do not nest tests unnecesarily

--- test/parser/rd-parser.spec.js
+++ test/parser/rd-parser.spec.js
@@ -19,52 +19,52 @@ const createController = (controllerType, ...args) =>
   Object.assign(Object.create(controllerType), ...args);
 
 describe('rd-parser', () => {
-
-  describe('simpleCondition', () => {
-
-    describe('field', () => {
-      it('errors when missing field', () => {
-        const field = parser.field.bind(null, neverMatchController);
-        expect(field).to.throw(/^Expected identifier/);
-      });
-
-      it('returns previous token when it matches IDENTIFIER', () => {
-        const previousToken = new Token('IDENTIFIER', 'n/a');
-        const controller = createController(alwaysMatchController, {
-          previous: () => previousToken
-        });
-        expect(parser.field(controller)).to.equal(previousToken);
-      });
+  describe('field', () => {
+    it('errors when missing field', () => {
+      const field = parser.field.bind(null, neverMatchController);
+      expect(field).to.throw(/^Expected identifier/);
     });
 
-    describe('operator', () => {
-      it('errors when missing operator', () => {
-        const operator = parser.operator.bind(null, neverMatchController);
-        expect(operator).to.throw(/^Expected operator/);
+    it('returns previous token when it matches IDENTIFIER', () => {
+      const previousToken = new Token('IDENTIFIER', 'n/a');
+      const controller = createController(alwaysMatchController, {
+        previous: () => previousToken
       });
-
-      it('returns previous when matches', () => {
-        const previousToken = new Token('OPERATOR', 'n/a');
-        const controller = createController(alwaysMatchController, {
-          previous: () => previousToken
-        });
-        expect(parser.operator(controller)).to.equal(previousToken);
-      });
-    });
-
-    describe('operand', () => {
-      it('errors when no match', () => {
-        const operand = parser.operand.bind(null, neverMatchController);
-        expect(operand).to.throw(/^Expected operand/);
-      });
-
-      it('returns previous when match', () => {
-        const previousToken = new Token('STRING', '123');
-        const controller = createController(alwaysMatchController, {
-          previous: () => previousToken
-        });
-        expect(parser.operand(controller)).to.equal(previousToken);
-      });
+      expect(parser.field(controller)).to.equal(previousToken);
     });
   });
+
+  describe('operator', () => {
+    it('errors when missing operator', () => {
+      const operator = parser.operator.bind(null, neverMatchController);
+      expect(operator).to.throw(/^Expected operator/);
+    });
+
+    it('returns previous when matches', () => {
+      const previousToken = new Token('OPERATOR', 'n/a');
+      const controller = createController(alwaysMatchController, {
+        previous: () => previousToken
+      });
+      expect(parser.operator(controller)).to.equal(previousToken);
+    });
+  });
+
+  describe('operand', () => {
+    it('errors when no match', () => {
+      const operand = parser.operand.bind(null, neverMatchController);
+      expect(operand).to.throw(/^Expected operand/);
+    });
+
+    it('returns previous when match', () => {
+      const previousToken = new Token('STRING', '123');
+      const controller = createController(alwaysMatchController, {
+        previous: () => previousToken
+      });
+      expect(parser.operand(controller)).to.equal(previousToken);
+    });
+  });
+
+  describe('simpleCondition', () => {
+    it('work');
+   });
 });
