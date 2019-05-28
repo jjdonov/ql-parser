e5d1127979ed420e47a4186847fd5e19e15de189
John Donovan <jjdonov@gmail.com>
2019-05-28 15:22:15 -0700

    Forgot to prettify index

--- index.js
+++ index.js
@@ -1,32 +1,35 @@
+/*eslint-disable no-console*/
 const ASTPrinter = require('./src/visitor/ast-printer.js');
 const Scanner = require('./src/scanner');
 const Parser = require('./src/parser');
 const Interpreter = require('./src/visitor/interpreter.js');
 
 function parse(input) {
-
-  const seed = [{
-    poNumber: "123",
-    orderTerms: {
-      orderDate: {
-        Issue: '2017-15-21'
+  const seed = [
+    {
+      poNumber: '123',
+      orderTerms: {
+        orderDate: {
+          Issue: '2017-15-21'
+        }
+      }
+    },
+    {
+      poNumber: '456',
+      orderTerms: {
+        orderDate: {
+          Issue: '2017-15-20'
+        }
       }
     }
-  }, {
-    poNumber: "456",
-    orderTerms: {
-      orderDate: {
-        Issue: '2017-15-20'
-      }
-    }
-  }];
+  ];
 
-  try{
+  try {
     console.log(`Parsing input: ${input}`);
     console.log('Operating on:\n' + JSON.stringify(seed, null, 4));
     const tokens = new Scanner(input).scan();
     tokens.forEach((t, i) => {
-      if(!t.type) {
+      if (!t.type) {
         console.log(`t${i} is malformed`);
         console.log(JSON.stringify(t));
       }
@@ -38,21 +41,19 @@ function parse(input) {
                 ${ASTPrinter.visit(ast)}`);
     console.log('EVAL');
     const fun = Interpreter.interpret(ast);
-    const result =seed.filter(fun);
+    const result = seed.filter(fun);
     console.log(' Result: ' + JSON.stringify(result, null, 4));
-  } catch(e) {
+  } catch (e) {
     console.log(`*** FAILURE *** ->
                 ${e.stack}`);
   }
 }
 
-function err() {
-
-}
-
-function scan(input) {
-  return new Scanner(input).scan();
-}
+//function err() {}
+//
+//function scan(input) {
+//  return new Scanner(input).scan();
+//}
 
 //console.log(`Results
 //           ${scan('poNumber = "12345"')}
@@ -61,7 +62,7 @@ function scan(input) {
 //           ${parse('poNumber = "123"')}`);
 
 //parse('poNumber = "123"');
-parse('orderTerms.orderDate.Issue = \'2017-15-21\'');
+parse("orderTerms.orderDate.Issue = '2017-15-21'");
 //console.log('\n------\n');
 //parse('poNumber = "123" OR poNumber = "456"');
 //console.log('\n------\n');
